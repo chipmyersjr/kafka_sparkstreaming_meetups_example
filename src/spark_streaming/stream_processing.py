23e977b7ec5be7f6c5c21264ace8d8aec6b8557e
Chip <chip.myersjr@gmail.com>
2018-11-17 13:54:29 -0800

    filtering out invalid records

--- src/spark_streaming/stream_processing.py
+++ src/spark_streaming/stream_processing.py
@@ -1,21 +1,36 @@
-import sys
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import json
+from json.decoder import JSONDecodeError
 
 
-if __name__ == "__main__":
+def main():
     sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
     ssc = StreamingContext(sc, 5)
 
     kafkaStream = KafkaUtils.createDirectStream(ssc, ["meetups"], {"metadata.broker.list": "172.20.0.5:9092"})
 
-    rsvps = kafkaStream.map(lambda message: json.loads(message[1]))
+    rsvps = kafkaStream.map(lambda message: parse_rsvp(message))
     rsvps.count().map(lambda x: 'RSVPS in this batch: %s' % x).pprint()
 
-    responses = rsvps.map(lambda rsvp: rsvp['response'])
+    responses = rsvps.map(lambda rsvp: rsvp.get("response") or "invalid").filter(lambda response: response != 'invalid')
     responses.pprint()
 
     ssc.start()
     ssc.awaitTermination()
+
+
+def parse_rsvp(message):
+    try:
+        rsvp = json.loads(message[1])
+        _ = rsvp["response"]
+    except JSONDecodeError:
+        return json.loads("{}")
+
+    return rsvp
+
+
+if __name__ == "__main__":
+    main()
+
