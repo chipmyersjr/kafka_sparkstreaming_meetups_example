6a8cb05f794e2ef8b2fb409dee8a6d24179ceefa
Chip <chip.myersjr@gmail.com>
2018-11-17 15:10:09 -0800

    converting state to dictionary

--- src/spark_streaming/stream_processing.py
+++ src/spark_streaming/stream_processing.py
@@ -8,14 +8,14 @@ from json.decoder import JSONDecodeError
 def main():
     sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
     ssc = StreamingContext(sc, 5)
+    ssc.checkpoint("/home/data")
+    sc.setLogLevel("OFF")
 
-    kafkaStream = KafkaUtils.createDirectStream(ssc, ["meetups"], {"metadata.broker.list": "172.20.0.5:9092"})
+    kafka_stream = KafkaUtils.createDirectStream(ssc, ["meetups"], {"metadata.broker.list": "172.20.0.5:9092"})
 
-    rsvps = kafkaStream.map(lambda message: parse_rsvp(message))
-    rsvps.count().map(lambda x: 'RSVPS in this batch: %s' % x).pprint()
+    rsvps = kafka_stream.map(lambda message: parse_rsvp(message))
 
-    responses = rsvps.map(lambda rsvp: rsvp.get("response") or "invalid").filter(lambda response: response != 'invalid')
-    responses.pprint()
+    count_by_responses(rsvps=rsvps)
 
     ssc.start()
     ssc.awaitTermination()
@@ -29,6 +29,26 @@ def parse_rsvp(message):
         return json.loads("{}")
 
     return rsvp
+
+
+def count_by_responses(rsvps):
+    responses = rsvps.map(lambda rsvp: rsvp.get("response") or "invalid") \
+                     .filter(lambda response: response != 'invalid') \
+                     .map(lambda response: (response, 1))
+
+    def update_function(new_values, running_count):
+        if running_count is None:
+            running_count = 0
+        return sum(new_values, running_count)
+
+    def convert_response_counts_to_dictionary(rdd):
+        response_count_dict = rdd.collectAsMap()
+        print(response_count_dict)
+
+    response_counts = responses.updateStateByKey(update_function)
+    response_counts.foreachRDD(convert_response_counts_to_dictionary)
+
+    return None
 
 
 if __name__ == "__main__":
