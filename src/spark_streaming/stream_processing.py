959288034943e883296fd182eeada0dec2c34d73
Chip <chip.myersjr@gmail.com>
2018-11-18 11:42:27 -0800

    addin global variables for config info

--- src/spark_streaming/stream_processing.py
+++ src/spark_streaming/stream_processing.py
@@ -5,14 +5,20 @@ import json
 from json.decoder import JSONDecodeError
 import redis
 
+KAKFA_MEETUPS_TOPIC = "meetups"
+KAFKA_BROKER = "172.20.0.5:9092"
+SPARK_STREAMING_CHECKPOINT_DIR = "/home/data"
+REDIS_HOST = "172.20.0.7"
+REDIS_PORT = "6379"
+
 
 def main():
     sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
-    ssc = StreamingContext(sc, 5)
-    ssc.checkpoint("/home/data")
+    ssc = StreamingContext(sc, 1)
+    ssc.checkpoint(SPARK_STREAMING_CHECKPOINT_DIR)
     sc.setLogLevel("OFF")
 
-    kafka_stream = KafkaUtils.createDirectStream(ssc, ["meetups"], {"metadata.broker.list": "172.20.0.5:9092"})
+    kafka_stream = KafkaUtils.createDirectStream(ssc, [KAKFA_MEETUPS_TOPIC], {"metadata.broker.list": KAFKA_BROKER})
 
     rsvps = kafka_stream.map(lambda message: parse_rsvp(message))
 
@@ -34,8 +40,8 @@ def count_by_responses(rsvps):
 
     def send_updated_results_to_redis(rdd):
         response_count_json = json.dumps(rdd.collectAsMap())
-        r = get_redis_connection()
-        r.set('CountByResponse', response_count_json)
+        redis_conn = get_redis_connection()
+        redis_conn.set('CountByResponse', response_count_json)
 
     response_counts = responses.updateStateByKey(update_function)
     response_counts.foreachRDD(send_updated_results_to_redis)
@@ -44,7 +50,7 @@ def count_by_responses(rsvps):
 
 
 def get_redis_connection():
-    return redis.Redis(host='172.20.0.7', port=6379)
+    return redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 
 def parse_rsvp(message):
