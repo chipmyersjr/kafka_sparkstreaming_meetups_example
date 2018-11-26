f440e483a99ee599909a018212c32b4197c1c124
Chip <chip.myersjr@gmail.com>
2018-11-25 16:15:09 -0800

    building view to read redis data

--- src/webapp/mysite/dashboard/views.py
+++ src/webapp/mysite/dashboard/views.py
@@ -1,5 +1,20 @@
+from django.template.loader import get_template
 from django.http import HttpResponse
+from redis import Redis
+import json
 
 
 def index(request):
-    return HttpResponse("This is the dashboard")
+    redis_conn = get_redis_connection()
+
+    response = redis_conn.get('CountByResponse')
+    response_json = json.loads(response.decode("utf-8"))
+
+    template = get_template("dashboard.html")
+    html = template.render(response_json)
+
+    return HttpResponse(html)
+
+
+def get_redis_connection():
+    return Redis(host="172.20.0.7", port=6379)
