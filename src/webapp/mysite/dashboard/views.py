07a3e830a43b2e2b027720b18c252830955e76ba
Chip <chip.myersjr@gmail.com>
2018-12-01 15:45:13 -0800

    adding redis host variable

--- src/webapp/mysite/dashboard/views.py
+++ src/webapp/mysite/dashboard/views.py
@@ -2,6 +2,9 @@ from django.template.loader import get_template
 from django.http import HttpResponse
 from redis import Redis
 import json
+import os
+
+REDIS_HOST = "172.20.0.7"
 
 
 def index(request):
@@ -10,11 +13,11 @@ def index(request):
     response = redis_conn.get('CountByResponse')
     response_json = json.loads(response.decode("utf-8"))
 
-    template = get_template("dashboard.html")
+    template = get_template("dashboard_example.html")
     html = template.render(response_json)
 
     return HttpResponse(html)
 
 
 def get_redis_connection():
-    return Redis(host="172.20.0.7", port=6379)
+    return Redis(host=REDIS_HOST)
